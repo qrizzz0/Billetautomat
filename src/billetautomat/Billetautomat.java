d1521ac7d7cdea266b5ae25c3aa90464c250573f
Anders <Anders@AndersPC>
2019-02-19 16:52:39 +0100

    Billetautomat

--- src/billetautomat/Billetautomat.java
+++ src/billetautomat/Billetautomat.java
@@ -4,6 +4,9 @@ public class Billetautomat {
   public static void main(String[] args){
     while(true){
       System.out.println("All hail Senpai <3 ");
+      
+        System.out.println("All hail Senpai :( ");
+      
     }
   }
 }
