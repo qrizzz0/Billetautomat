a37626d974c4e8efb1eb9a98cdcfae4fcdd2c9c5
Kris <Kris@DESKTOP-5GNL4QT.eduroam.wireless.dtu.dk>
2019-03-04 15:59:57 +0100

    Catch exceptions bedre?

--- src/billetautomat/WriteToFile.java
+++ src/billetautomat/WriteToFile.java
@@ -4,9 +4,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class WriteToFile {
-    private FileWriter writer;
+        private FileWriter writer;
     
-    public WriteToFile(String FileName) throws IOException {
+        public WriteToFile(String FileName) {
         try {
         writer = new FileWriter(FileName, true); //Opret fil. Catch exception hvis noget går galt
         }
@@ -16,17 +16,28 @@ public class WriteToFile {
         }       
     }
     
-    public void LogToFile(String input) throws IOException {
+    public void LogToFile(String input) {
         Timestamp time = new Timestamp(System.currentTimeMillis());
-        writer.flush();
-        writer.append(time + ": " + input + "\n");
-        writer.close();
+        try {
+            writer.flush();
+            writer.append(time + ": " + input + "\n");
+            writer.close();
+        }
+        catch (IOException ex) {
+            ex.getStackTrace();
+        }
     }
     
-    public void WriteToFile(String input) throws IOException {
-        writer.flush();
-        writer.append(input);
-        writer.close();
+    public void WriteToFile(String input) {
+        try {
+            writer.flush();
+            writer.append(input);
+            writer.close();
+        }
+        catch (IOException ex) {
+            ex.getStackTrace();
+        }
     }
+
     
 }
