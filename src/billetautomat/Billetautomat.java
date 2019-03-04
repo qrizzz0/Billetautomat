9e02966409e0e72b899f9e87ad844b432e069d0f
gitte <gitte@DESKTOP-ME8G2JG.clients.wireless.dtu.dk>
2019-03-04 15:07:39 +0100

    Deler koden op i objekter.

--- src/billetautomat/Billetautomat.java
+++ src/billetautomat/Billetautomat.java
@@ -49,29 +49,6 @@ public class Billetautomat {
         balance = scan.nextDouble();
     }
 
-    public static void udskrivning() {
-        System.out.println("  @@@@@@@@@@@@@@@@@@@@@@  ");
-        System.out.println("@@ Borgen Trafikselskab @@");
-        System.out.println("  @@@@@@@@@@@@@@@@@@@@@@  ");
-        System.out.println("@@                      @@");
-        System.out.println("@@          @@          @@");
-        System.out.println("@@         @@@@         @@");
-        System.out.println("@@        @ BT @        @@");
-        System.out.println("@@         @@@@         @@");
-        System.out.println("@@          @@          @@");
-        System.out.println("@@                      @@");
-        System.out.println("@@        Billet        @@");
-        System.out.println("@@                      @@");
-        System.out.println("@@         Pris         @@");
-        System.out.println("@@                      @@");
-        System.out.println("@@      Andet info      @@");
-        System.out.println("@@                      @@");
-        System.out.println("@@                      @@");
-        System.out.println("@@                      @@");
-        System.out.println("@@                      @@");
-        System.out.println("  @@@@@@@@@@@@@@@@@@@@@@  ");
-        System.out.println();
-    }
 
     private static void getTicketInfo() {
         System.out.println("\n#################################");
@@ -117,87 +94,7 @@ public class Billetautomat {
         }
     }
     //For nu har jeg blot lavet nogle billettyper da jeg ikke helt ved hvordan det med zonerne fungere.
-     public static void købBillet() {
 
-                System.out.println(" ");
-                System.out.println("Indtast beløb til køb af billetter: ");
-            
-                    Scanner beløb = new java.util.Scanner(System.in);
-                    int sum=beløb.nextInt();
-            int voksen=30;
-            int barn=15;
-            int hund=15;
-            int cykel=10;
-                        
-
-
-        Scanner valg = new java.util.Scanner(System.in);
-     int valget = -1;
-
-    while(valget < 1 || valget > 5){
-            //selve menuen
-                System.out.println("Vælg billettype");
-                System.out.println("1. Voksenbillet");
-                System.out.println("2. Børnebillet");
-                System.out.println("3. Hundebillet");
-                System.out.println("4. Cykelbillet");
-                System.out.println("5. Afslut");
-                
-            if(valg.hasNextInt())
-            valget = valg.nextInt();
-
-    }
-
-     switch(valget){
-        case 1:
-            if(sum>=voksen){
-                System.out.println("Du har købt 1 voksenbillet");
-                sum=sum-voksen;
-                System.out.println("Du får: "+sum+"kr. tilbage");
-                Billetautomat.udskrivning();
-            }
-           break;
-        case 2:
-            if(sum>=barn){
-                System.out.println("Du har købt 1 børnebillet");
-                sum=sum-barn;
-                System.out.println("Du får: "+sum+"kr. tilbage");
-                Billetautomat.udskrivning();
-            }
-           break;
-        case 3:
-            if(sum>=hund){
-                System.out.println("Du har købt 1 hundebillet");
-                sum=sum-hund;
-                System.out.println("Du får: "+sum+"kr. tilbage");
-                Billetautomat.udskrivning();
-            }
-           break;
-        case 4:
-            if(sum>=cykel){
-                System.out.println("Du har købt 1 cykelbillet");
-                sum=sum-cykel;
-                System.out.println("Du får: "+sum+"kr. tilbage");
-                Billetautomat.udskrivning();
-            }
-            break;
-        case 5:
-            System.exit(0);
-            break;
-   }
-     }
-     
-     private static void velkommen(){
-                System.out.println("  @@@@@@@@@@@@@@@@@@@@@@  ");
-                System.out.println("@@ Borgen Trafikselskab @@");
-                System.out.println("  @@@@@@@@@@@@@@@@@@@@@@  ");
-                System.out.println(" ");
-                System.out.println("Billetautomat til køb af billet");
-                System.out.println("-Voksenbillet koster 30kr. (18+år)");
-                System.out.println("-Børnebillet koster 15kr. (6-17år)");
-                System.out.println("-Hundebillet koster 15kr.");
-                System.out.println("-Cykelbillet koster 10kr.");
-     }
      
 
 
